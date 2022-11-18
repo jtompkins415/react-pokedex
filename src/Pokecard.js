7558a10e7e365d13eba25294d48e22178eaf077f
jtompkins415 <john.tompkins415@gmail.com>
2022-11-18 14:22:54 -0800

    create api variable, and render elements with classes

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,13 @@
-const Pokecard = ({name, image, type, exp}) => {
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+const Pokecard = (props) => {
+    let pokeImg = `${POKE_API}${props.id}.png`;
     return (
-        <div>
-            <h3>{name}</h3>
-            <img src={image} alt="Poke Img"/>
-            <p>{type}</p>
-            <p>{exp}</p>
+        <div className="Pokecard">
+            <h3 className="Pokecard-name">{props.name}</h3>
+            <img className="Pokecard-img" src={pokeImg} alt="Poke Img"/>
+            <p className="Pokecard-type">Type: {props.type}</p>
+            <p className="Pokecard-exp">EXP: {props.exp}</p>
         </div>
     )
 }
